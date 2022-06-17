07e5c1f4422f1893621d71e00ebc4b393ef9c8fc
tracybcarpenter <103546803+tracybcarpenter@users.noreply.github.com>
2022-06-16 22:35:45 -0400

    hello orb

---
+++ packages/nouns-webapp/src/components/ShortAddress/index.tsx
@@ -0,0 +1,35 @@
+import { useReverseENSLookUp } from '../../utils/ensLookup';
+import { useEthers } from '@usedapp/core';
+import Davatar from '@davatar/react';
+import classes from './ShortAddress.module.css';
+import { containsBlockedText } from '../../utils/moderation/containsBlockedText';
+
+export const useShortAddress = (address: string) => {
+  return address && [address.substr(0, 4), address.substr(38, 4)].join('...');
+};
+
+const ShortAddress: React.FC<{ address: string; avatar?: boolean; size?: number }> = props => {
+  const { address, avatar, size = 24 } = props;
+  const { library: provider } = useEthers();
+
+  const ens = useReverseENSLookUp(address);
+  const ensMatchesBlocklistRegex = containsBlockedText(ens || '', 'en');
+  const shortAddress = useShortAddress(address);
+
+  if (avatar) {
+    return (
+      <div className={classes.shortAddress}>
+        {avatar && (
+          <div key={address}>
+            <Davatar size={size} address={address} provider={provider} />
+          </div>
+        )}
+        <span>{ens && !ensMatchesBlocklistRegex ? ens : shortAddress}</span>
+      </div>
+    );
+  }
+
+  return <>{ens ? ens : shortAddress}</>;
+};
+
+export default ShortAddress;
---
+++ packages/nouns-webapp/src/components/Winner/index.tsx
@@ -0,0 +1,93 @@
+import { Button, Row, Col } from 'react-bootstrap';
+import { useAppSelector } from '../../hooks';
+import classes from './Winner.module.css';
+import ShortAddress from '../ShortAddress';
+import clsx from 'clsx';
+import { isMobileScreen } from '../../utils/isMobile';
+
+interface WinnerProps {
+  winner: string;
+  isNounders?: boolean;
+}
+
+const Winner: React.FC<WinnerProps> = props => {
+  const { winner, isNounders } = props;
+  const activeAccount = useAppSelector(state => state.account.activeAccount);
+
+  const isCool = useAppSelector(state => state.application.isCoolBackground);
+  const isMobile = isMobileScreen();
+
+  const isWinnerYou =
+    activeAccount !== undefined && activeAccount.toLocaleLowerCase() === winner.toLocaleLowerCase();
+
+  const nonNounderNounContent = isWinnerYou ? (
+    <Row className={classes.youSection}>
+      <Col lg={4} className={classes.youCopy}>
+        <h2
+          className={classes.winnerContent}
+          style={{
+            color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+          }}
+        >
+          You
+        </h2>
+      </Col>
+      {!isMobile && (
+        <Col>
+          <a
+            href="https://nouns.center/nouners"
+            target="_blank"
+            rel="noreferrer noopener"
+            className={classes.verifyLink}
+          >
+            <Button className={classes.verifyButton}>What now?</Button>
+          </a>
+        </Col>
+      )}
+    </Row>
+  ) : (
+    <ShortAddress size={40} address={winner} avatar={true} />
+  );
+
+  const nounderNounContent = <h2>nounders.eth</h2>;
+
+  return (
+    <>
+      <Row className={clsx(classes.wrapper, classes.section)}>
+        <Col xs={1} lg={12} className={classes.leftCol}>
+          <h4
+            style={{
+              color: isCool ? 'var(--brand-cool-light-text)' : 'var(--brand-warm-light-text)',
+            }}
+          >
+            Winner
+          </h4>
+        </Col>
+        <Col xs="auto" lg={12}>
+          <h2
+            className={classes.winnerContent}
+            style={{
+              color: isCool ? 'var(--brand-cool-dark-text)' : 'var(--brand-warm-dark-text)',
+            }}
+          >
+            {isNounders ? nounderNounContent : nonNounderNounContent}
+          </h2>
+        </Col>
+      </Row>
+      {isWinnerYou && isMobile && (
+        <Row>
+          <a
+            href="https://nouns.center/nouners"
+            target="_blank"
+            rel="noreferrer noopener"
+            className={classes.verifyLink}
+          >
+            <Button className={classes.verifyButton}>What now?</Button>
+          </a>
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default Winner;
